463a4a29f06628f978b9ee300456e27419501b0c
안혜연 <ahy020606@naver.com>
2024-03-22 01:59:42 +0900

    docs: 할 일 목록 리스트 컴포넌트 분리

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import plusImg from '../assets/img/plus.svg';
 
 const InputStyle = styled.input`
   background-color: #d9d9d9d9;
@@ -7,18 +8,18 @@ const InputStyle = styled.input`
   outline: none;
   border-radius: 10px;
   padding: 5px 5px 5px 30px; /*텍스트와 이미지 겹침 방지를 위한 패딩 조정*/
-  width: 345px;
+  width: 340px;
   height: 20px;
   line-height: 20px;
   cursor: pointer;
 
-  background-image: url('../assets/img/plus.svg');
+  background-image: url('${plusImg}');
   background-position: 5px center; /*이미지 위치 조정*/
   background-repeat: no-repeat;
   background-size: 20px 20px;
 `;
 
-export default function ToDoInput({ onAddTodo }) {
+export default function InputForm({ onAddTodo }) {
   const [value, setValue] = useState('');
 
   const handleInput = (event) => {
@@ -28,13 +29,13 @@ export default function ToDoInput({ onAddTodo }) {
   const handleSubmit = (event) => {
     event.preventDefault();
     if (!value.trim()) return; // 공백 입력 방지
-    onAddTodo(value); // 부모 컴포넌트의 함수 호출
-    setValue(''); // 입력 필드 초기화
+    onAddTodo(value);
+    setValue('');
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <input
+      <InputStyle
         type="text"
         placeholder="할 일을 입력하세요."
         value={value}
---
+++ src/components/List.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import styled from 'styled-components';
+import deleteBtn from '../assets/img/trash.svg';
+import grayCheckBtn from '../assets/img/grayCheck.svg';
+
+const ToDoBlock = styled.div`
+  background-color: white;
+  border-radius: 10px;
+  max-width: 375px;
+  height: 510px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  overflow-y: auto;
+`;
+
+const ItemContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px 10px 10px 30px;
+  position: relative;
+
+  &::after {
+    content: ''; /* 가상 요소에 내용이 없음을 명시 */
+    position: absolute; /* 상위 요소인 ItemContainer에 대해 절대 위치 설정 */
+    bottom: 0; /* 하단에 위치 */
+    left: 0; /* 왼쪽 시작점 */
+    width: calc(100% - 30px); /* 전체 너비에서 20px만큼 줄임 */
+    margin-left: 30px; /* 왼쪽 여백 설정 */
+    border-bottom: 1px solid #ccc; /* 하단 테두리 스타일 */
+  }
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+`;
+
+const DeleteButtonImg = styled.img`
+  cursor: pointer;
+  width: 20px;
+  height: 20px;
+`;
+
+const CheckButtonImg = styled.img`
+  width: 20px;
+  height: 20px;
+  cursor: pointer;
+  margin-right: 7px;
+`;
+
+export default function List({ toDoData, setToDoData }) {
+  const handleClick = (id) => {
+    let newToDoData = toDoData.filter((data) => data.id !== id);
+    setToDoData(newToDoData);
+  };
+
+  return (
+    <ToDoBlock>
+      {toDoData.map((data) => (
+        <ItemContainer key={data.id}>
+          {data.title}
+          <ButtonsContainer>
+            <CheckButtonImg src={grayCheckBtn} alt="회색 체크 버튼" />
+            <DeleteButtonImg
+              src={deleteBtn}
+              alt="삭제 버튼"
+              onClick={() => handleClick(data.id)}
+            />
+          </ButtonsContainer>
+        </ItemContainer>
+      ))}
+    </ToDoBlock>
+  );
+}
